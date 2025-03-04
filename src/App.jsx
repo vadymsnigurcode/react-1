884ecbd80f72935206e8bf86d4e311b7d06b0412
Vadym <vadym.snigur.code@gmail.com>
2025-03-05 00:06:12 +0200

    refactoring todoitem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import "./styles.css"
 import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 
 export default function App() {
   // const [newItem, setNewItem] = useState("")
@@ -63,28 +64,7 @@ export default function App() {
   <>
   <NewTodoForm onSubmit={addTodo}/>
   <h1 className="header"> Todo List</h1>
-  <ul className="list">
-    {todos.length === 0 && "No Todos"}
-    {todos.map(todo => {
-      return (
-      <li key={todos.id}>
-      <label>
-        <input 
-        type="checkbox" 
-        checked={todo.completed}
-        onChange={e=> toggleTodo(todo.id, e.target.checked)}/>
-        {todo.title}
-      </label>
-      <button 
-        onClick={()=>deleteTodo(todo.id)} 
-        className="btn btn-danger">
-        Delete
-      </button>
-    </li>
-      )
-    })}
-
-   </ul>
+  <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
   </>
   )
 }
