5fcf329725762c48e9b10f821c038418cb31122e
Vadym <vadym.snigur.code@gmail.com>
2025-03-04 23:47:24 +0200

    TodoForm refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react"
 import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
+  // const [newItem, setNewItem] = useState("")
   const [todos,setTodos] = useState([])
   // setNewItem ("asdfasdf")
 
+  function addTodo(title) {
+        setTodos(currentTodos => {
+          return [
+          ...currentTodos,
+          {id: crypto.randomUUID(), title, completed: false},
+          ]
+        })
+  }
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -52,16 +61,7 @@ export default function App() {
 
   return (
   <>
-  <form onSubmit={handleSubmit} className ="new-item-form" >
-    <div className="form-row">
-      <label htmlFor="item"> New Item</label>
-      <input 
-        value={newItem}
-        onChange={e=>setNewItem(e.target.value)}
-        type="text" id="item"/>
-    </div>
-    <button className="btn">add</button>
-  </form>
+  <NewTodoForm onSubmit={addTodo}/>
   <h1 className="header"> Todo List</h1>
   <ul className="list">
     {todos.length === 0 && "No Todos"}
---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+export function NewTodoForm(props) {
+    props.onSubmit
+
+  const [newItem, setNewItem] = useState("")
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    
+        // setTodos(currentTodos => {
+        //   return [
+        //   ...currentTodos,
+        //   {id: crypto.randomUUID(), title: newItem, completed: false},
+        //   ]
+        // })
+
+        if (newItem === "") return 
+
+        props.onSubmit(newItem)
+    
+        setNewItem("")
+    
+        // ------------------------
+        // set second item example
+        // ------------------------
+        // setTodos(currentTodos => {
+        //   return [
+        //   ...currentTodos,
+        //   {id: crypto.randomUUID(), title: newItem, completed: false},
+        //   ]
+        // })
+      }
+
+
+    return (
+    <form onSubmit={handleSubmit} className ="new-item-form" >
+    <div className="form-row">
+      <label htmlFor="item"> New Item</label>
+      <input 
+        value={newItem}
+        onChange={e=>setNewItem(e.target.value)}
+        type="text" id="item"/>
+    </div>
+    <button className="btn">add</button>
+  </form>
+  )
+}
